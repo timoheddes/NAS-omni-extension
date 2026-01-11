0b2caaf45fd3f19ff8a7b7821b62966e99dbdb8b
theddes <79908211+timoheddes@users.noreply.github.com>
2026-01-11 14:09:02 +0100

    tweak console log statements

--- shared/ui/console.js
+++ shared/ui/console.js
@@ -2,7 +2,7 @@ export const OMNI_ASCII_LOGO = '⫷ ⦿ ⫸';
 
 export const printToConsole = (text) => {
   console.log(
-    `${OMNI_ASCII_LOGO} ${text}`,
+    `%c${OMNI_ASCII_LOGO} - Omni ${text}%c`,
     'color: #ffffffff; background: #2c3e50'
   );
 };
